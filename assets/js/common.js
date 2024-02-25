17874e8a3508178944f1dd464fbc8c0021f31060
kimkyungm13 <kyung_m13@naver.com>
2024-02-25 22:27:40 +0900

    scd

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,13 +4,10 @@ window.addEventListener('wheel', function (event) {
         // 마우스 휠을 아래로 스크롤할 때 실행할 코드
         $('header').addClass('wheel');
         $('.menu-wrap').css('display', 'block');
-        $('.all-more').click(function () {
-            $('.menu-nav').toggleClass('active');
-            $('.menu-more').toggleClass('active')
-        })
         // 여기에 아래로 스크롤할 때 실행할 동작을 추가합니다.
     }
 });
+
 // 스크롤 이벤트에 대한 핸들러 추가
 window.addEventListener('scroll', function () {
     var scrollPosition = window.scrollY;
@@ -19,7 +16,19 @@ window.addEventListener('scroll', function () {
         // 스크롤이 최상단에 도달했을 때 실행할 코드
         $('header').removeClass('wheel');
         $('.menu-wrap').css('display', '');
-
         // 여기에 최상단에 도달했을 때 실행할 동작을 추가합니다.
     }
+});
+
+// .all-more 요소의 클릭 이벤트 핸들러 (이벤트 위임)
+$(document).on('click', '.all-more', function () {
+    var menuNav = $('.menu-nav');
+    if (menuNav.css('display') === 'none') {
+        // 현재 메뉴가 숨겨진 상태일 때
+        menuNav.css('display', 'block');
+    } else {
+        // 현재 메뉴가 표시된 상태일 때
+        menuNav.css('display', 'none');
+    }
+    $('.menu-more').toggleClass('active');
 });
