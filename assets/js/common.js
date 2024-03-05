460c18eb08ebbd1dbd164edfb95ee96319a09138
kimkyungm13 <kyung_m13@naver.com>
2024-03-05 14:22:33 +0900

    .

--- assets/js/common.js
+++ assets/js/common.js
@@ -29,17 +29,6 @@ $(function () {
         }
         lastScroll = curr
     });
-
-    // 마우스 휠 이벤트에 대한 핸들러 추가
-    window.addEventListener('wheel', function (event) {
-        if (event.deltaY > 0) {
-            // 마우스 휠을 아래로 스크롤할 때 실행할 코드
-            $('header').addClass('wheel');
-            $('.menu-wrap').css('display', 'block');
-            // 여기에 아래로 스크롤할 때 실행할 동작을 추가합니다.
-        }
-    });
-
     // 스크롤 이벤트에 대한 핸들러 추가
     window.addEventListener('scroll', function () {
         var scrollPosition = window.scrollY;
